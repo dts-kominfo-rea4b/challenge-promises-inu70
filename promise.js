90234d4403c2db0ff88fbdfd904085abd0b2723b
inu70 <ibnusalam70@gmail.com>
2022-09-18 22:34:20 +0700

    add promise output

--- promise.js
+++ promise.js
@@ -1,6 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
+const promiseOutput = (output) => {
+  return new Promise((marah, tidak_marah) => {
+    if (output == 'World of the married' || output == 'Nanti Kita Cerita Tentang Hari Ini') {
+      console.log(`marah`);
+    } else {
+      console.log(`tidak marah`);
+    }
+  });
+};
 const promiseOutput = null;
 
 module.exports = {
